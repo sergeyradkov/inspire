11862d5e64a1df051ae0aa6bedbf1d11c61d98b1
sergeyradkov <radkov@gmail.com>
2016-05-03 09:29:38 -0600

    Add messages

--- app/components/time/time-controller.js
+++ app/components/time/time-controller.js
@@ -5,11 +5,12 @@ app.controller('TimeCtrl', function($scope, $interval){
     
     $scope.clock = Date.now();
     var d = new Date();
+    debugger
     var h = d.getHours();
-    if(h < 5 || h > 21) time.mes = "Good night, Idaho!";
-    if(h > 5  && h < 12 ) part = "Good morning, Idaho";
-    if(h > 12 && h < 17 ) time.mes = "Good afternoon, Idaho";
-    if(h > 17 && h < 21 ) part = "Good evening, Idaho";    
+    if(h < 5 || h > 21)   time.mes = "Good night, Idaho!";
+    if(h > 5  && h < 12 ) time.mes = "Good morning, Idaho!";
+    if(h > 12 && h < 17 ) time.mes = "Good afternoon, Idaho!";
+    if(h > 17 && h < 21 ) time.mes = "Good evening, Idaho!";    
   }
   
    tick();
