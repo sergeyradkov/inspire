df4e0f24ada451624c655df59346eb3028f45686
sergeyradkov <radkovsn@gmail.com>
2016-06-03 18:06:38 -0600

    123

--- components/time.js
+++ components/time.js
@@ -3,18 +3,16 @@ app.controller('TimeCtrl', function($scope, $interval, WeatherService){
   
 	WeatherService.getWeather().then(function(weather){
 	time.city = weather.name;
-	debugger
 	})
 
-  
   var tick = function() {
     $scope.clock = Date.now();
     var d = new Date();
     var h = d.getHours();
-    if(h < 5 || h > 21)   time.mes = "Good night, " + time.city + "!";
-    if(h > 5  && h < 12 ) time.mes = "Good morning " + time.city + "!";
-    if(h > 12 && h < 17 ) time.mes = "Good afternoon " + time.city + "!";
-    if(h > 17 && h < 21 ) time.mes = "Good evening " + time.city + "!";    
+    if(h < 5  || h > 21 ) time.mes = "Good night, " + time.city + "!";
+    if(h > 5  && h < 12 ) time.mes = "Good morning, " + time.city + "!";
+    if(h > 12 && h < 17 ) time.mes = "Good afternoon, " + time.city + "!";
+    if(h > 17 && h < 21 ) time.mes = "Good evening, " + time.city + "!";    
   }
   
    tick();
